adfb8b83b8f15368b6608fd11144edb75b0fa620
Omar Abdelwahab <omar.abdelwahab.info@gmail.com>
2020-08-26 06:33:18 +0200

    created a warn level command

---
+++ Commands/warnlevel.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+
+
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have the required permission or moderater role.");
+    
+    let wUser = message.mentions.users.first();
+
+    if(!wUser) return message.reply("User not found.");
+  
+    if(!warns[wUser.id]) return message.reply("User has no Warnings");
+   
+    else {
+        let warnlevel = warns[wUser.id].warns;
+
+        message.reply(`<@${wUser.id}> has ${warnlevel} warnings`); 
+    }
+}
+
+module.exports.help = {
+    name: "warnlevel"
+}
