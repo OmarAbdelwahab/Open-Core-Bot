1099084a49c2688656f49adef293545fa0e09c15
Omar Abdelwahab <omar.abdelwahab.info@gmail.com>
2020-08-27 22:00:16 +0200

    tempmute function needs refactoring

--- Commands/tempmute.js
+++ Commands/tempmute.js
@@ -35,7 +35,8 @@ module.exports.run = async (bot, message, args) => {
     let mutetime = args[1];
    
     if(!mutetime) return message.reply("You didn't specify a time!");
-
+    
+    // all this shit has to be refactored down below
     await(tomute.addRole(muterole.id));
    
     message.reply(`<@{tomute.id} has been muted for ${ms(ms(mutetime))}`);
