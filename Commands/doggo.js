842b8a5c893b4efe41263d79cf030ef206821481
Omar Abdelwahab <omar.abdelwahab.info@gmail.com>
2020-08-28 04:46:59 +0200

    SuperAgent Demo

---
+++ Commands/doggo.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+//SuperAgent doggo demo
+module.exports.run = async (bot, message, args) => {
+    let {body} = await superagent
+    .get(`https://random.dog/woof.json`);
+
+
+    let dogembed = new Discord.MessageEmbed()
+    .setColor("#ff9900")
+    .setTitle("Doggo")
+    .setImage(body.url);
+
+    message.channel.send(dogembed);
+}
+
+module.exports.help = {
+    name: "doggo"
+}
+
