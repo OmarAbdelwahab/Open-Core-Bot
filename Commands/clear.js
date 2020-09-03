99e60b3fdcd4fd483e7f203f7df030e175f17cbc
Omar Abdelwahab <omar.abdelwahab.info@gmail.com>
2020-09-03 04:19:30 +0200

    initial commit

---
+++ Commands/clear.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    if(!message.member.hasPermission("MANAGE_MESSSAGES")) return message.reply("You don't have the required permissions");
+    if(!args[0]) return message.channel.send("Please enter the number messages to delete");
+
+    message.channel.bulkDelete(args[0]).then(()=>{
+        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    });
+}
+
+module.exports.help = {
+    name: "clear"
+}
