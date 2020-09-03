7002cbc000b989899c761f306d330a4e67408539
Omar Abdelwahab <omar.abdelwahab.info@gmail.com>
2020-09-03 04:45:10 +0200

    message delete now takes an object in discord v12

--- Commands/clear.js
+++ Commands/clear.js
@@ -6,8 +6,8 @@ module.exports.run = async(bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSSAGES")) return message.reply("You don't have the required permissions");
     if(!args[0]) return message.channel.send("Please enter the number messages to delete");
 
-    message.channel.bulkDelete(args[0]).then(()=>{
-        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    message.channel.bulkDelete(args[0]).then(()=> {
+        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete({timeout: 5000}));
     });
 }
 
